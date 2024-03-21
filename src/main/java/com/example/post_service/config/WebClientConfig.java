479ccb1cdacc51a4bb7c2af0efd636165771eab5
= <=>
2024-03-20 17:07:27 -0700

    bug fixing

--- src/main/java/com/example/post_service/config/WebClientConfig.java
+++ src/main/java/com/example/post_service/config/WebClientConfig.java
@@ -1,4 +1,4 @@
-package com.example.post_service.consfig;
+package com.example.post_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
